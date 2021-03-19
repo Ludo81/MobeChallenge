251b80a470b156c04d1ba5804ace832c57f84431
Ludo81 <ludovic.tournier@hotmail.fr>
2021-03-19 08:50:36 +0100

    Structure project #2

---
+++ app/src/main/java/end_page/EndActivity.java
@@ -0,0 +1,16 @@
+package end_page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobechallengeproject.R;
+
+public class EndActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end);
+    }
+}
