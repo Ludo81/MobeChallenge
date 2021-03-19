4034be45ebaa3cef6ced03feb1f7a8ce042e6386
AurelienPLT <35218076+AurelienPLT@users.noreply.github.com>
2021-03-19 10:07:44 +0100

    Suppression ancienne musique

--- app/src/main/java/end_page/EndActivity.java
+++ app/src/main/java/end_page/EndActivity.java
@@ -2,9 +2,12 @@ package end_page;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import com.example.mobechallengeproject.R;
+
+import level_page.gameplay.LevelGamePlayActivity;
 
 public class EndActivity extends AppCompatActivity {
 
@@ -12,5 +15,8 @@ public class EndActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
+
+        MediaPlayer music = MediaPlayer.create(EndActivity.this, R.raw.end_music);
+        music.start();
     }
 }
