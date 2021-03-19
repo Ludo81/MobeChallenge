72f698306e73eb91366cce0f8a1740cfa486cc63
AurelienPLT <35218076+AurelienPLT@users.noreply.github.com>
2021-03-19 09:46:30 +0100

    Ajout musique

--- app/src/main/java/level_page/gameplay/LevelGamePlayActivity.java
+++ app/src/main/java/level_page/gameplay/LevelGamePlayActivity.java
@@ -2,6 +2,7 @@ package level_page.gameplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import com.example.mobechallengeproject.R;
@@ -12,5 +13,8 @@ public class LevelGamePlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_game_play);
+
+        MediaPlayer ring= MediaPlayer.create(LevelGamePlayActivity.this, R.raw.century_fox_flute);
+        ring.start();
     }
 }
