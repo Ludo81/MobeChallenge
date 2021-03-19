0e2d5a3f2b2b65b64bb6bfeae9c776bfb808045d
AurelienPLT <35218076+AurelienPLT@users.noreply.github.com>
2021-03-19 11:16:21 +0100

    Ajout background

--- app/src/main/java/level_page/gameplay/LevelGamePlayActivity.java
+++ app/src/main/java/level_page/gameplay/LevelGamePlayActivity.java
@@ -2,11 +2,13 @@ package level_page.gameplay;
 
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
@@ -23,16 +25,34 @@ public class LevelGamePlayActivity extends Activity implements SensorEventListen
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        int width = size.x;
+        int height = size.y;
+
+        LevelView.yMax = height+80;
+        LevelView.xMax = width;
+
+        LevelView.activity = this;
+
         LevelView levelView = new LevelView(this);
         setContentView(levelView);
+
+
 
         MediaPlayer music = MediaPlayer.create(LevelGamePlayActivity.this, R.raw.level_music);
         music.start();
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
-
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        switch(sensorEvent.sensor.getType()) {
+            case Sensor.TYPE_LIGHT:
+                LevelView.luminosite = sensorEvent.values[0];
+                break;
+        }
     }
 
     @Override
